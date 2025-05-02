2973a57f80a3a9b01ae01d56684cc49991177280
Ellyse <ellyse@common.tools>
2025-05-01 20:57:37 -0700

    do not run message processor if the message.content is not a list - this happens when there is a tool call

--- lib/chains/llm_chain.ex
+++ lib/chains/llm_chain.ex
@@ -893,9 +893,9 @@ defmodule LangChain.Chains.LLMChain do
           Message.t() | {:halted, Message.t(), Message.t()}
   def run_message_processors(
         %LLMChain{message_processors: processors} = chain,
-        %Message{role: :assistant} = message
+        %Message{role: :assistant, content: message_content} = message
       )
-      when is_list(processors) and processors != [] do
+      when is_list(processors) and processors != [] and is_list(message_content) and message_content != [] do
     # start `processed_content` with the message's content as a string
     Logger.debug("llmchain.run_message_processors")
     message = %Message{message | processed_content: ContentPart.parts_to_string(message.content)}
@@ -928,6 +928,7 @@ defmodule LangChain.Chains.LLMChain do
 
   # the message is not an assistant message. Skip message processing.
   def run_message_processors(%LLMChain{} = _chain, %Message{} = message) do
+    Logger.debug("llmchain.run_message_processors: not processing this message")
     message
   end
 
